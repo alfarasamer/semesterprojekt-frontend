8a276facf5e7fcade9ac270e6e8e3a917c381965
alfarasamer <alfarasamer@gmail.com>
2022-01-06 01:58:20 +0100

    Added Delete function and autoupdate for Brands

--- js/brand.js
+++ js/brand.js
@@ -1,3 +1,37 @@
+
+
+
+
+function listAllBrands() {
+    $.getJSON('http://localhost:8080/brands', function (data) {
+        $.each(data, function (index, brand) {
+            var id = brand.id;
+            var brandName = brand.brandName;
+            $('.output').append('<tr><th class="brand-id" scope="row">' + id +
+                '</th><td class="brand-name">'
+                + brandName + '</td><td>Edit - <input type="button" value="clickme" onclick="deleteBrandById('+id+')" /></td></tr>');
+        });
+    });
+}
+
+listAllBrands();
+
+
+
+function deleteBrandById(id) {
+    $.ajax({
+        type: "DELETE",
+        url: 'http://localhost:8080/brands/'+id,
+        dataType: "json",
+        statusCode: {
+            200: function() {
+            $('#brand')[0].reset();
+            $('.output').replaceWith('<tbody class="output">'+listAllBrands()+'</tbody>')
+            }
+          }
+    })
+}
+
 $(document).ready(function() {
     $('#brand').submit(function(e) {
         e.preventDefault();
@@ -8,29 +42,12 @@ $(document).ready(function() {
             dataType: "json",
             //contentType: 'application/x-www-form-urlencoded',
             data: {brandName:$("#brand-name").val()},
-                       /* success: function(data)
-            {
-                if (data === 'Correct') {
-                    window.location.replace('admin/admin.php');
+            statusCode: {
+                200: function() {
+                $('#brand')[0].reset();
+                $('.output').replaceWith('<tbody class="output">'+listAllBrands()+'</tbody>')
                 }
-                else {
-                    alert(data);
-                }
-            }*/
-        });
-    });
-    
+              }
+        })
+    })
 })
-
-$.getJSON('http://localhost:8080/brands', function (data) {
-    $.each(data,function (index, brand) {
-        var id = brand.id;
-        var brandName = brand.brandName;
-
-        $('.output').append('<tr><th class="brand-id" scope="row">'+id+'</th><td class="brand-name">'+brandName+'</td><td>Edit - Delete</td></tr>')
-
-        
-    });
-});
-
-
